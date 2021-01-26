81d84d8d4e81fd9f66fca0ed78cc263b74561e8a
HasegawaShuhei <HasegawaShuhei@users.noreply.github.com>
2021-01-26 20:47:51 +0900

    appbar完成

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,11 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          toolbarHeight: 100,
-          leading: FlatButton(child: Icon(Icons.home),
-            onPressed: (){
-            //todo
+          // toolbarHeight: 100,
+          leading: IconButton (
+            icon: Icon(Icons.home),
+            onPressed: () {
+            //  todo
             },
           ),
           title: Text("口座"),
@@ -36,6 +37,21 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ],
+          bottom: PreferredSize(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Text("残高合計", style: TextStyle(fontSize: 20),),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 60),
+                    child: Text("""100,000,000円""", style: TextStyle(fontSize: 36),),
+                  ),
+                ],
+              ),
+              preferredSize: Size.fromHeight(kToolbarHeight)),
         ),
       ),
     );
