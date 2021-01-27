f41ec3bc983f8f7153fa14772615047fa0ef3be7
HasegawaShuhei <HasegawaShuhei@users.noreply.github.com>
2021-01-27 20:09:38 +0900

    完成（文字のズレ要修正）

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,17 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.white
-      ),
+      theme: ThemeData(primaryColor: Colors.white),
       home: Scaffold(
         appBar: AppBar(
           // toolbarHeight: 100,
-          leading: IconButton (
+          leading: IconButton(
             icon: Icon(Icons.home),
             onPressed: () {
-            //  todo
+              //  todo
             },
           ),
           title: Text("口座"),
@@ -28,9 +25,11 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 FlatButton.icon(
-                  icon: Icon(
-                      Icons.refresh_rounded),
-                  label: Text("更新", style: TextStyle(fontSize: 12),),
+                  icon: Icon(Icons.refresh_rounded),
+                  label: Text(
+                    "更新",
+                    style: TextStyle(fontSize: 12),
+                  ),
                   onPressed: () {
                     //todo
                   },
@@ -44,11 +43,17 @@ class MyApp extends StatelessWidget {
                 children: [
                   Container(
                     padding: EdgeInsets.only(left: 20),
-                    child: Text("残高合計", style: TextStyle(fontSize: 20),),
+                    child: Text(
+                      "残高合計",
+                      style: TextStyle(fontSize: 20),
+                    ),
                   ),
                   Container(
                     padding: EdgeInsets.only(left: 60),
-                    child: Text("""100,000,000円""", style: TextStyle(fontSize: 36),),
+                    child: Text(
+                      """100,000,000円""",
+                      style: TextStyle(fontSize: 36),
+                    ),
                   ),
                 ],
               ),
@@ -57,7 +62,7 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.only(left:12, top: 12, right: 12),
+              padding: const EdgeInsets.only(left: 12, top: 12, right: 12),
               child: Container(
                 height: 128,
                 width: double.infinity,
@@ -65,10 +70,54 @@ class MyApp extends StatelessWidget {
                   color: Colors.blueAccent,
                   borderRadius: BorderRadius.circular(20),
                 ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16, top: 12, right: 12),
+                          child: Text("代表口座", style: TextStyle(color: Colors.white),),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 184, top: 12, right: 8, bottom: 8),
+                          child: Text("2021/01/27日時点", style: TextStyle(color: Colors.white),),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          children: [
+                            Text("山田太郎 様", style: TextStyle(color: Colors.white),),
+                            Text("インド支店", style: TextStyle(color: Colors.white),),
+                            Padding(
+                              padding: const EdgeInsets.only(left:12),
+                              child: Text("普通 0123456", style: TextStyle(color: Colors.white),),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 260),
+                              child: Icon(Icons.arrow_forward_ios, color: Colors.white,),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 120, top: 4),
+                              child: Text("50,000,000円",
+                                style: TextStyle(color: Colors.white, fontSize: 24),),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left:12, top: 12, right: 12),
+              padding: const EdgeInsets.only(left: 12, top: 12, right: 12),
               child: Container(
                 height: 128,
                 width: double.infinity,
@@ -76,31 +125,76 @@ class MyApp extends StatelessWidget {
                   color: Colors.cyan,
                   borderRadius: BorderRadius.circular(20),
                 ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16, top: 12, right: 12),
+                          child: Text("代表口座", style: TextStyle(color: Colors.white),),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 184, top: 12, right: 8, bottom: 8),
+                          child: Text("2021/01/27日時点", style: TextStyle(color: Colors.white),),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          children: [
+                            Text("山田太郎 様", style: TextStyle(color: Colors.white),),
+                            Text("スイス支店", style: TextStyle(color: Colors.white),),
+                            Padding(
+                              padding: const EdgeInsets.only(left:12),
+                              child: Text("普通 0123456", style: TextStyle(color: Colors.white),),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 260),
+                              child: Icon(Icons.arrow_forward_ios, color: Colors.white,),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 120, top: 4),
+                              child: Text("50,000,000円",
+                                style: TextStyle(color: Colors.white, fontSize: 24),),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-
             Padding(
-              padding: const EdgeInsets.only(left:12, top: 12, right: 12),
+              padding: const EdgeInsets.only(left: 12, top: 12, right: 12),
               child: DottedBorder(
                 borderType: BorderType.RRect,
                 radius: Radius.circular(20),
-                  child:
-                    Container(
-                      height: 100,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
+                child: Container(
+                  height: 100,
+                  width: double.infinity,
+                  decoration: BoxDecoration(),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.add,
+                        size: 36,
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.add, size: 36,),
-                          Text("アカウントに口座を追加する",
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ],
+                      Text(
+                        "アカウントに口座を追加する",
+                        style: TextStyle(fontSize: 16),
                       ),
-                    ),
+                    ],
                   ),
+                ),
+              ),
             ),
           ],
         ),
